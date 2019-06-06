b5a83d8387cb77d6d77b6abf6baf11e421be98ef
Oghenero-Sigbenu <sigbenuoghenero@gmail.com>
2019-06-06 15:10:17 +0100

    made some changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
     
-let mobileToggle = document.getElementById('mobile-nav-toggle');
-let mobileDropdown = document.getElementById('mobile-nav-dropdown');
+var menu = document.getElementById("hamb-menu");
+var details = document.getElementById("left-menu");
 
-mobileToggle.addEventListener('click', function () {
-  mobileToggle.classList.toggle('open');
-  mobileDropdown.classList.toggle('hide-dropdown');
-})
+//on clocking on hamb-menu it should change it class 
+menu.addEventListener("click", () => {
+    menu.classList.toggle("change")
+    console.log("love")
+    if(details.style.display === 'block'){
+        details.style.display = 'none';
+    }else{
+        details.style.display = 'block';
+    };
+
+} )
